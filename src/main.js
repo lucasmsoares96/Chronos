3cbe479b4bfa9f4697ca54a0300369e74b0f4bce
lucasmsoares96 <lucasmsoares96@gmail.com>
2020-02-13 18:20:06 -0300

    criação da store

--- src/main.js
+++ src/main.js
@@ -6,10 +6,12 @@ import App from './App.vue'
 
 import './plugins/bootstrap-vue'
 import router from './plugins/router'
+import store from './plugins/store'
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   router,
   render: h => h(App),
 }).$mount('#app')
