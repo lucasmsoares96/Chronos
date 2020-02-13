23f530083d18d9998654af23de789b98ecb4ae66
lucasmsoares96 <lucasmsoares96@gmail.com>
2020-02-13 18:20:06 -0300

    criação da store

--- src/components/schedule/Schedule.vue
+++ src/components/schedule/Schedule.vue
@@ -1,68 +1,22 @@
 <template>
     <div id="schedule">
         <h1>Quadro de horarios</h1>
-        <b-table bordered :items="items" :fields="fields"></b-table>
+        <b-table class="schedule" bordered :items="items" :fields="fields">
+        </b-table>
     </div>
 </template>
 
 <script>
-import axios from "axios";
 export default {
     name: "Schedule",
-    data() {
-        return {
-            items: [],
-            fields: []
-        };
-    },
-    methods: {
-        loadSchedules() {
-            axios.get("http://localhost:3003/schedule").then(res => {
-                this.items = res.data;
-                this.items.forEach((e, index1) => {
-                    this.items[index1]["_cellVariants"] = {};
-                    Object.values(e).forEach((el, index2) => {
-                        if (
-                            el == 2 &&
-                            Object.keys(this.items[index1])[index2] != "idhorario"
-                        ) {
-                            let col = String(
-                                Object.keys(this.items[index1])[index2]
-                            );
-                            this.items[index1]["_cellVariants"][col] = "danger";
-                        } else if (
-                            el == 1 &&
-                            Object.keys(this.items[index1])[index2] != "idhorario"
-                        ) {
-                            let col = String(
-                                Object.keys(this.items[index1])[index2]
-                            );
-                            this.items[index1]["_cellVariants"][col] = "warning";
-                        } else if (
-                            el == 0 &&
-                            Object.keys(this.items[index1])[index2] != "idhorario"
-                        ) {
-                            let col = String(
-                                Object.keys(this.items[index1])[index2]
-                            );
-                            this.items[index1]["_cellVariants"][col] = "info";
-                        }
-                    });
-                });
-                this.fields = Object.keys(this.items[0]).filter(obj => {
-                    return obj == "idhorario" || obj == "_cellVariants"
-                        ? false
-                        : true;
-                });
-            });
+    computed: {
+        fields(){
+            return this.$store.state.fields
+        },
+        items (){
+            return this.$store.state.items
         }
-        // acao() {
-        //     console.log("deu certo");
-        // }
     },
-    mounted() {
-        this.loadSchedules();
-    }
 };
 </script>
 
---
+++ src/plugins/store.js
@@ -0,0 +1,58 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from "axios";
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+    state: {
+        items: [],
+        fields: []
+    },
+    mutations: {
+        resSchedule(state){
+            axios.get("http://localhost:3003/schedule").then(res => {
+                state.items = res.data;
+                state.items.forEach((e, index1) => {
+                    state.items[index1]["_cellVariants"] = {};
+                    Object.values(e).forEach((el, index2) => {
+                        if (
+                            el == 2 &&
+                            Object.keys(state.items[index1])[index2] !=
+                                "idhorario"
+                        ) {
+                            let col = String(
+                                Object.keys(state.items[index1])[index2]
+                            );
+                            state.items[index1]["_cellVariants"][col] = "danger";
+                        } else if (
+                            el == 1 &&
+                            Object.keys(state.items[index1])[index2] !=
+                                "idhorario"
+                        ) {
+                            let col = String(
+                                Object.keys(state.items[index1])[index2]
+                            );
+                            state.items[index1]["_cellVariants"][col] =
+                                "warning";
+                        } else if (
+                            el == 0 &&
+                            Object.keys(state.items[index1])[index2] !=
+                                "idhorario"
+                        ) {
+                            let col = String(
+                                Object.keys(state.items[index1])[index2]
+                            );
+                            state.items[index1]["_cellVariants"][col] = "info";
+                        }
+                    });
+                });
+                state.fields = Object.keys(state.items[0]).filter(obj => {
+                    return obj == "idhorario" || obj == "_cellVariants"
+                        ? false
+                        : true;
+                });
+            });
+        }
+    }
+})
