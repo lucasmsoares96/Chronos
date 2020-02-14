82107d438086a12e8ee8c3c15be69c0dfadeea89
lucasmsoares96 <lucasmsoares96@gmail.com>
2020-02-14 20:46:53 -0300

    enviar recurso e horario para o backend

--- src/components/schedule/Schedule.vue
+++ src/components/schedule/Schedule.vue
@@ -7,41 +7,58 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
     name: "Schedule",
     methods: {
-        clickOnCell(e){
-            console.log(e)
+        clickOnCell(e) {
+            // console.log([e.srcElement.parentNode.rowIndex]);
+            // console.log([e.srcElement.cellIndex]);
+            const table = document.getElementsByTagName("table")[0]
+            let row = table.rows[e.srcElement.parentNode.rowIndex].cells[0].innerHTML
+            let column = table.rows[0].cells[e.srcElement.cellIndex].innerHTML
+            console.log('linha  '+ row)
+            console.log('coluna '+ column)
+            axios.post("http://localhost:3000/professorhorario",{
+                data: this.data,
+                recurso : row,
+                horario : column
+            })
         },
-        catchCell(){
-            const tdInfo = [... document.querySelectorAll('td.table-info')]
+        catchCell() {
+            const tdInfo = [...document.querySelectorAll("td.table-info")];
             tdInfo.forEach(e => {
-                e.innerHTML = ''
-                e.onclick = this.clickOnCell
-                })
-            const tdwarning = [... document.querySelectorAll('td.table-warning')]
+                e.innerHTML = "";
+                e.onclick = this.clickOnCell;
+            });
+            const tdwarning = [
+                ...document.querySelectorAll("td.table-warning")
+            ];
             tdwarning.forEach(e => {
-                e.innerHTML = ''
-                e.onclick = this.clickOnCell
-                })
-            const tdDanger = [... document.querySelectorAll('td.table-danger')]
+                e.innerHTML = "";
+                e.onclick = this.clickOnCell;
+            });
+            const tdDanger = [...document.querySelectorAll("td.table-danger")];
             tdDanger.forEach(e => {
-                e.innerHTML = ''
-                e.onclick = this.clickOnCell
-                })
+                e.innerHTML = "";
+                e.onclick = this.clickOnCell;
+            });
         }
     },
     computed: {
-        fields(){
-            return this.$store.state.fields
+        fields() {
+            return this.$store.state.fields;
         },
-        items (){
-            return this.$store.state.items
+        items() {
+            return this.$store.state.items;
+        },
+        data() {
+            return this.$store.state.obj.data;
         }
     },
-    mounted(){
-        this.catchCell(),
-        this.$store.commit('resSchedule')
+    mounted() {
+        this.catchCell(), this.$store.commit("resSchedule");
     }
 };
 </script>
