24060fad3e759a1e5ea638d3f814772480294fa2
Vitor Martins Soares <vitor231408@gmail.com>
2020-02-13 22:37:30 -0300

    correcaoMudancaDeTela

--- src/plugins/store.js
+++ src/plugins/store.js
@@ -1,8 +1,8 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 import axios from "axios";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
@@ -10,49 +10,58 @@ export default new Vuex.Store({
         fields: []
     },
     mutations: {
-        resSchedule(state){
-            axios.get("http://localhost:3003/schedule").then(res => {
-                state.items = res.data;
-                state.items.forEach((e, index1) => {
-                    state.items[index1]["_cellVariants"] = {};
-                    Object.values(e).forEach((el, index2) => {
-                        if (
-                            el == 2 &&
-                            Object.keys(state.items[index1])[index2] !=
-                                "idhorario"
-                        ) {
-                            let col = String(
-                                Object.keys(state.items[index1])[index2]
-                            );
-                            state.items[index1]["_cellVariants"][col] = "danger";
-                        } else if (
-                            el == 1 &&
-                            Object.keys(state.items[index1])[index2] !=
-                                "idhorario"
-                        ) {
-                            let col = String(
-                                Object.keys(state.items[index1])[index2]
-                            );
-                            state.items[index1]["_cellVariants"][col] =
-                                "warning";
-                        } else if (
-                            el == 0 &&
-                            Object.keys(state.items[index1])[index2] !=
-                                "idhorario"
-                        ) {
-                            let col = String(
-                                Object.keys(state.items[index1])[index2]
-                            );
-                            state.items[index1]["_cellVariants"][col] = "info";
-                        }
+        resSchedule(state, obj) {
+            axios
+                .get("http://localhost:3000/dataRecursos", {
+                    params: {
+                        data : obj[0],
+                        tipoRecurso : obj[1]
+                    }
+                })
+                .then(res => {
+                    state.items = res.data;
+                    state.items.forEach((e, index1) => {
+                        state.items[index1]["_cellVariants"] = {};
+                        Object.values(e).forEach((el, index2) => {
+                            if (
+                                el == 2 &&
+                                Object.keys(state.items[index1])[index2] !=
+                                    "idhorario"
+                            ) {
+                                let col = String(
+                                    Object.keys(state.items[index1])[index2]
+                                );
+                                state.items[index1]["_cellVariants"][col] =
+                                    "danger";
+                            } else if (
+                                el == 1 &&
+                                Object.keys(state.items[index1])[index2] !=
+                                    "idhorario"
+                            ) {
+                                let col = String(
+                                    Object.keys(state.items[index1])[index2]
+                                );
+                                state.items[index1]["_cellVariants"][col] =
+                                    "warning";
+                            } else if (
+                                el == 0 &&
+                                Object.keys(state.items[index1])[index2] !=
+                                    "idhorario"
+                            ) {
+                                let col = String(
+                                    Object.keys(state.items[index1])[index2]
+                                );
+                                state.items[index1]["_cellVariants"][col] =
+                                    "info";
+                            }
+                        });
+                    });
+                    state.fields = Object.keys(state.items[0]).filter(obj => {
+                        return obj == "idhorario" || obj == "_cellVariants"
+                            ? false
+                            : true;
                     });
                 });
-                state.fields = Object.keys(state.items[0]).filter(obj => {
-                    return obj == "idhorario" || obj == "_cellVariants"
-                        ? false
-                        : true;
-                });
-            });
         }
     }
-})
+});
