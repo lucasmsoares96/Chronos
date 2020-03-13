27ec3033c47edfab0fe331adfa6a23e5664f630e
lucasmsoares96 <lucasmsoares96@gmail.com>
2020-03-13 10:42:58 -0300

    dropDown login

--- src/global.js
+++ src/global.js
@@ -3,14 +3,14 @@ import Vue from 'vue'
 export const baseApiUrl = 'http://localhost:3000'
 export const userKey = '__reserva_recurso'
 
-export function showError(e){
-    if(e && e.response && e.reponse.data){
-        Vue.toasted.global.defaultError({msg: e.response.data})
-    }else if (typeof e === 'string'){
-        Vue.toasted.global.defaultError({msg: e})
+export function showError(e) {
+    if (e && e.response && e.response.data) {
+        Vue.toasted.global.defaultError({ msg: e.response.data })
+    } else if (typeof e === 'string') {
+        Vue.toasted.global.defaultError({ msg: e })
     } else {
         Vue.toasted.global.defaultError()
     }
 }
 
-export default {baseApiUrl, showError}
+export default { baseApiUrl, showError }
